b967723d77934c262a0f2e90659da419284d8d41
gerardor0814 <gerardor0814@gmail.com>
2024-11-26 22:35:03 -0600

    remove prints

--- src/Main.java
+++ src/Main.java
@@ -41,9 +41,7 @@ public class Main {
             GraphBuilder graphBuilder = new GraphBuilder(parser.getHead());
             graphBuilder.build();
             graphBuilder.addPriorities();
-            System.err.println(graphBuilder.makeDotFile());
             graphBuilder.schedule();
-            System.out.println("done");
         } else {
             System.err.println("Due to syntax errors, run terminates");
         }
